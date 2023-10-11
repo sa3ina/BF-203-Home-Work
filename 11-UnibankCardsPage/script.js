feec515507db999ba775607087245123e79212c2
sa3ina <118393529+sa3ina@users.noreply.github.com>
2023-10-11 13:27:17 +0400

    Add files via upload

---
+++ 11-UnibankCardsPage/script.js
@@ -0,0 +1,35 @@
+// let message = "salam";
+// message = "sagol";
+// console.log(message)
+
+// let number = 16;
+// let floatnum = 0.5;
+// console.log(number, floatnum)
+
+// let x = true;
+// let y = false;
+// console.log(x,y)
+
+// object
+// const person = {firstName:"Name", lastName:"Surname"};
+
+// array
+// const cars = ["alma", "armud", "heyva"];
+
+// let cavab= 5+"salam"
+// console.log(cavab)
+
+// let cavab =8+"salam"+10
+// console.log(cavab)
+
+// let cavab ="salam"+10+"sagol"
+// console.log(cavab)
+
+// let cavab = 3 + 4 + "salam" + 5;
+// console.log(cavab);
+
+// console.log(typepof cavab)
+
+// console.log(typeof " ")
+
+// alert("hey")
